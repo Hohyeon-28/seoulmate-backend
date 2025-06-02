8edcc71285bf94a02716ab575a6224f9363831ea
HoHyeon <zadachi@naver.com>
2025-06-02 10:38:33 +0900

    middleware added

--- app/main.py
+++ app/main.py
@@ -3,10 +3,18 @@ from app.api import place
 from app.api import event
 from app.api import interest
 from app.api import auth
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(place.router, prefix="/api")
 app.include_router(event.router, prefix="/api") 
 app.include_router(interest.router)
 app.include_router(auth.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],      
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
